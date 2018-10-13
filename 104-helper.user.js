9f393390672d1306abbc1e2990f4fe64941bf33e
Gea-Suan Lin <gslin@gslin.org>
2018-10-13 16:42:22 +0800

    Refactor and add ptt links.

--- 104-helper.user.js
+++ 104-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/104-helper-userscript
 // @description Add useful links to 104 job pages.
 // @include     https://www.104.com.tw/*
-// @version     0.20181013.0
+// @version     0.20181013.1
 // @license     MIT
 // ==/UserScript==
 
@@ -18,11 +18,19 @@
 
         let qollie_link = 'https://www.qollie.com/search?keyword=' + encodeURIComponent(company_name) + '&kind=company';
 
-        let el = document.createElement('a');
-        el.setAttribute('href', qollie_link);
-        el.innerHTML = '去 Qollie 看看';
+        let qollie_el = document.createElement('a');
+        qollie_el.setAttribute('href', qollie_link);
+        qollie_el.setAttribute('style', 'margin-right: 1em;');
+        qollie_el.innerHTML = '去 Qollie 看看';
+        company_el.parentElement.appendChild(qollie_el);
 
-        company_el.parentElement.appendChild(el);
+        let ptt_link = 'https://www.google.com/search?q=' + encodeURIComponent(company_name) + '+site:www.ptt.cc';
+        let ptt_el = document.createElement('a');
+        ptt_el.setAttribute('href', ptt_link);
+        ptt_el.setAttribute('style', 'margin-right: 1em;');
+        ptt_el.innerHTML = '去 Ptt 看看 (by Google)';
+        company_el.parentElement.appendChild(ptt_el);
+
         return;
     }
 
@@ -32,11 +40,19 @@
 
         let qollie_link = 'https://www.qollie.com/search?keyword=' + encodeURIComponent(company_name) + '&kind=company';
 
-        let el = document.createElement('a');
-        el.setAttribute('href', qollie_link);
-        el.innerHTML = '去 Qollie 看看';
+        let qollie_el = document.createElement('a');
+        qollie_el.setAttribute('href', qollie_link);
+        qollie_el.setAttribute('style', 'margin-right: 1em;');
+        qollie_el.innerHTML = '去 Qollie 看看';
+        company_el.parentElement.parentElement.appendChild(qollie_el);
 
-        company_el.parentElement.parentElement.appendChild(el);
+        let ptt_link = 'https://www.google.com/search?q=' + encodeURIComponent(company_name) + '+site:www.ptt.cc';
+        let ptt_el = document.createElement('a');
+        ptt_el.setAttribute('href', ptt_link);
+        ptt_el.setAttribute('style', 'margin-right: 1em;');
+        ptt_el.innerHTML = '去 Ptt 看看 (by Google)';
+        company_el.parentElement.parentElement.appendChild(ptt_el);
+
         return;
     }
 })();
