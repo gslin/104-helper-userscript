646c1d92a4b8b73232b3abbd8dc27b296ab51a4a
Gea-Suan Lin <gslin@gslin.org>
2018-10-14 23:46:09 +0800

    Add more filtering rules.

--- 104-helper.user.js
+++ 104-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/104-helper-userscript
 // @description Add useful links to 104 job pages.
 // @include     https://www.104.com.tw/*
-// @version     0.20181013.6
+// @version     0.20181013.7
 // @license     MIT
 // ==/UserScript==
 
@@ -13,6 +13,7 @@
     let company_name_normalize = function(name){
         return name
             .trim()
+            .replace(/^(台|臺)灣/, '')
             .replace(/^(法|英|)屬/, '')
             .replace(/^(開曼群島|英|美|港|香港)商/, '')
             .replace(/(台|臺)灣(子|分)公司$/, '')
