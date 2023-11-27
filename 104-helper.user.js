c957e1b595f7a2dfa518c5db068bfdd54938e149
Gea-Suan Lin <gslin@gslin.org>
2023-11-28 06:45:42 +0800

    Add 立樂高園股份有限公司 into hh_list.

--- 104-helper.user.js
+++ 104-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/104-helper-userscript
 // @description Add useful links to 104 job pages.
 // @include     https://www.104.com.tw/*
-// @version     0.20231115.0
+// @version     0.20231128.0
 // @license     MIT
 // @grant       GM_openInTab
 // @grant       GM_xmlhttpRequest
@@ -202,6 +202,7 @@
             '新加坡商艾得克有限公司台灣分公司',
             '益晟人力資源有限公司',
             '立展企管顧問有限公司',
+            '立樂高園股份有限公司',
             '立樂高園股份有限公司',
             '精英人力資源股份有限公司',
             '經緯智庫股份有限公司 (MGR Consulting Co., Ltd.)',
