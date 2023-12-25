93aaa0daa615e438f50bae559afc6fb651d4ee80
Gea-Suan Lin <gslin@gslin.org>
2023-12-26 07:48:15 +0800

    Add 萬通國際人力開發股份有限公司 to hh_list.

--- 104-helper.user.js
+++ 104-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/104-helper-userscript
 // @description Add useful links to 104 job pages.
 // @include     https://www.104.com.tw/*
-// @version     0.20231205.0
+// @version     0.20231226.0
 // @license     MIT
 // @grant       GM_openInTab
 // @grant       GM_xmlhttpRequest
@@ -209,6 +209,7 @@
             '經緯智庫股份有限公司 (MGR Consulting Co., Ltd.)',
             '聚賢亞洲有限公司',
             '萬寶華企業管理顧問股份有限公司',
+            '萬通國際人力開發股份有限公司',
             '藝珂人事顧問股份有限公司',
             '逸亞管理顧問股份有限公司',
             '首席國際獵才有限公司',
